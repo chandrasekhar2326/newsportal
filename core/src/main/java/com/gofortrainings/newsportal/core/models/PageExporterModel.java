9acce90030da36958aa260cec0fc843b062271a3
chandrasekhar2326 <chandrasekhapatchabati2326@gmail.com>
2025-03-17 14:53:39 +0530

    newsportal pageexporter component newsportal core

---
+++ core/src/main/java/com/gofortrainings/newsportal/core/models/PageExporterModel.java
@@ -0,0 +1,54 @@
+package com.gofortrainings.newsportal.core.models;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.*;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Model(adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = { "practice/components/practice" })
+@Exporter(name = "jackson", extensions = "json")
+public class PageExporterModel {
+
+    @Default(values="Test")
+    @Inject
+    @Named("title")
+    protected String heading;
+
+    @Inject
+    @Optional
+    protected String description;
+
+    @Inject
+    @Optional
+    protected String country;
+
+    @PostConstruct
+    protected void init() { }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
